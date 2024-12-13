d1bd97dfff6cebfaaf23be707d891e65b8ab8f20
wesleyara <wes1gom@gmail.com>
2024-12-13 09:00:20 -0300

    feat: add type string folder

---
+++ 003-type-string/main.py
@@ -0,0 +1,14 @@
+# aspas simples
+print('Olá, mundo')
+
+# aspas duplas
+print("Olá, mundo")
+
+# aspas triplas
+print('''Olá, mundo''')
+
+# escapando aspas
+print("Olá, \"mundo\"")
+
+# r para raw string
+print(r"Olá, \"mundo\"")
