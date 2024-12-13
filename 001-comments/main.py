54a76d0e53df81a8437e026e6e9665653d758f48
wesleyara <wes1gom@gmail.com>
2024-12-12 22:35:44 -0300

    feat: add comments folder

---
+++ 001-comments/main.py
@@ -0,0 +1,11 @@
+# Usamos o # para definir um comentário de linha
+
+"""
+Usamos 3 aspas duplas para definir um comentário de bloco
+Mais conhecido como docstring
+"""
+
+'''
+Usamos 3 aspas simples para definir um comentário de bloco
+Mais conhecido como docstring
+'''
